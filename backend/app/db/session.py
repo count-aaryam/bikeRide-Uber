5e62ef0a934a420e2572a010c68d7bdabb772ecf
Aaryam <aaryamsingh2005@gmail.com>
2026-05-13 16:31:37 +0530

    multiple changes required for setups

--- backend/app/db/session.py
+++ backend/app/db/session.py
@@ -1,8 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import declarative_base
 
-DATABASE_URL = "postgresql://postgres:password@localhost:5432/bike_app"
-
+DATABASE_URL = "postgresql://postgres:030405@localhost:5432/bike_app"
+Base = declarative_base()
 engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(
